8aa9e039796614e553ee926de6cb1ccff3dcdef7
Yuhao Tang <tangyh@umich.edu>
2017-08-24 00:29:12 +0800

    add findFirstLostInteger

--- array/main.cpp
+++ array/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <array>
 #include <vector>
 
 
@@ -38,11 +37,11 @@ void testDefiniteMode() {
 /*
  * assume that the neighbors in the array are not duplicate
  * return: a local maximum of the array
- * time complexity: O(log n)
+ * time complexity: O(log n), not definitely due to preprocessing for the array
  */
 int findLocalMaximum(std::vector<int> arr) {
     arr.push_back(INT_MIN);
-    arr.insert(arr.begin(), INT_MIN);
+    arr.insert(arr.begin(), INT_MIN); // cost O(N)
     int leftIdx = 0, rightIdx = arr.size();
     int mid;
     while (leftIdx < rightIdx - 1) {
@@ -61,7 +60,37 @@ void testFindLocalMaximum() {
     std::cout << findLocalMaximum(arr);
 }
 
+/*
+ * find the first lost integer start from 1
+ * example: 1 2 3 4 5 7 8, then the first lost positive integer should be 6
+ * note: the array is not garanteed to be sorted
+ *
+ * time complexity: O(N)
+ * space complexity: O(1)
+ */
+int findFirstLostInteger(std::vector<int> arr) {
+    int len = arr.size();
+    int i;
+    for (i = 0; i < len;) {
+        if (arr[i] == i + 1) {
+            i++;
+        } else if (arr[i] < i + 1 || arr[i] > len - 1 || arr[i] == arr[arr[i] - 1]) {
+            // delete
+            arr[i] = arr[--len];
+        } else { // arr[i] < i + 1
+            //swap
+            std::swap(arr[arr[i] - 1], arr[i]);
+        }
+    }
+    return i + 1;
+}
+
+void testFindFirstLostInteger() {
+    std::vector<int> arr({3, 5, 1, 2, -3, 7, 4, 8});
+    std::cout << findFirstLostInteger(arr);
+}
+
 int main() {
-    testFindLocalMaximum();
+    testFindFirstLostInteger();
     return 0;
 }
