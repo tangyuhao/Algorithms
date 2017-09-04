d1ca23be1a621ffcb9031db45689e2f57dc4b9e0
Yuhao Tang <tangyh@umich.edu>
2017-09-04 16:53:54 -0400

    add minChanges

--- README.md
+++ README.md
@@ -85,5 +85,6 @@ This is a personal practice for algorithms using c++11, I tried my best to not u
   - LIS with O(n^2) time complexity
   - LIS with O(nlogn) time complexity
   - max profit given stock prices
+  - minimum operations problem
 
 
--- dynamicProgramming/main.cpp
+++ dynamicProgramming/main.cpp
@@ -91,13 +91,9 @@ void testLongestIncreasingSubsequence2() {
  * Example: [3,6,2,3,6,7] and k = 2, then the max profit is 6 - 3 + 7 - 2 = 8
  */
 int maxProfitWithKDeals(std::vector<int> a, int k) {
-    std::vector<std::vector<int>> dp(k + 1, std::vector<int>(a.size()));
+    std::vector<std::vector<int>> dp(k + 1, std::vector<int>(a.size(), 0));
     std::vector<std::vector<int>> pre(k + 1, std::vector<int>(a.size(), -1));
     int size = (int) a.size();
-    for (int i = 0; i <= k; i++)
-        dp[i][0] = 0;
-    for (int i = 0; i < size; i++)
-        dp[0][i] = 0;
     for (int i = 1; i <= k; i++) {
         for (int j = 1; j < size; j++) {
             int mx = INT_MIN;
@@ -139,7 +135,31 @@ void testMaxProfitWithKDeals() {
     std::cout << maxProfitWithKDeals(a, 2);
 }
 
+/*
+ * given a number n, we need to caculate the number of changes for 1 to become n,
+ * each change allow us to plus 1 or time 2
+ *
+ * Note: this version is not the best, the best one is really easy: if the number is odd then number = number - 1;
+ * else: number = number / 2;
+ */
+int minChanges(int n, std::vector<int> &times) {
+    if (n == 1)
+        return 0;
+    if (times[n] != -1)
+        return times[n];
+    if (n % 2 == 0) {
+        times[n] = std::min(minChanges(n / 2, times), minChanges(n - 1, times)) + 1;
+    } else
+        times[n] = minChanges(n - 1, times) + 1;
+    return times[n];
+}
+
+void testMinChanges() {
+    std::vector<int> times(2016, -1);
+    std::cout << minChanges(2015, times);
+}
+
 int main() {
-    testMaxProfitWithKDeals();
+    testMinChanges();
     return 0;
 }
