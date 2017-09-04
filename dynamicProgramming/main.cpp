ccd82fe07c64e05a03385269ccb866450de830c5
Yuhao Tang <tangyh@umich.edu>
2017-09-04 10:55:05 -0400

    add LIS solution with O(nlogn) time complexity

--- dynamicProgramming/main.cpp
+++ dynamicProgramming/main.cpp
@@ -35,7 +35,56 @@ void testLongestIncreasingSubsequence() {
     std::cout << longestIncreasingSubsequence(a);
 }
 
+/*
+ * version 2: with time complexity O(nlogn)
+ */
+int longestIncreasingSubsequence2(std::vector<int> a) {
+    int size = (int) a.size();
+    std::vector<int> dp;
+    std::vector<int> pre(size, -1);
+    std::vector<int> idx;
+    dp.push_back(a[0]);
+    idx.push_back(0);
+    int cnt = 1;
+    for (int i = 1; i < size; i++) {
+        // binary search to find the last number who is less than a[i]
+        int start = 0, end = cnt - 1; //
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (dp[mid] >= a[i])
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        pre[i] = end < 0 ? -1 : idx[end];
+        if (end + 1 < cnt) {
+            dp[end + 1] = a[i];
+            idx[end + 1] = i;
+        } else {
+            dp.push_back(a[i]);
+            idx.push_back(i);
+            cnt++;
+        }
+    }
+    std::vector<int> result;
+    int i = idx[cnt - 1];
+    while (i >= 0) {
+        result.push_back(a[i]);
+        i = pre[i];
+    }
+    std::reverse(result.begin(), result.end());
+    std::for_each(result.begin(), result.end(), [](int k) { std::cout << k << " "; });
+    std::cout << std::endl;
+    return cnt;
+}
+
+void testLongestIncreasingSubsequence2() {
+    std::vector<int> a({2, 4, 2, 3, 6, 45, 23, 0, 3, 3, 4, 5, 7, 9});
+    std::cout << longestIncreasingSubsequence2(a);
+
+}
+
 int main() {
-    testLongestIncreasingSubsequence();
+    testLongestIncreasingSubsequence2();
     return 0;
 }
